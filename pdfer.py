0730dc7e28fe2821ffb43fc054ba87d20e954ee5
rmungar <mgraul@iesrafaelalberti.es>
2025-03-08 18:17:27 +0100

    Automatico

---
+++ pdfer.py
@@ -0,0 +1,19 @@
+import fpdf
+
+class Pdfer():
+
+
+    def crearInforme1(self):
+        pass
+
+    def crearInforme2(self):
+        pass
+
+    def crearInforme3(self):
+        pass
+
+    def crearInforme4(self):
+        pass
+
+    def crearInforme5(self):
+        pass
---
+++ pdfs.py
@@ -0,0 +1,32 @@
+from fpdf import FPDF
+
+
+class PDF1(FPDF):
+    def header(self):
+        pass
+    def footer(self):
+        pass
+
+class PDF2(FPDF):
+    def header(self):
+        pass
+    def footer(self):
+        pass
+
+class PDF3(FPDF):
+    def header(self):
+        pass
+    def footer(self):
+        pass
+
+class PDF4(FPDF):
+    def header(self):
+        pass
+    def footer(self):
+        pass
+
+class PDF5(FPDF):
+    def header(self):
+        pass
+    def footer(self):
+        pass
